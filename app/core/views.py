a873a226287593a9008354913d7dccef692bf652
tnummy@gmail.com <tnummy@gmail.com>
2017-03-15 17:52:37 -0400

    update for dynamic active tab styling

--- app/core/views.py
+++ app/core/views.py
@@ -8,7 +8,7 @@ mod = Blueprint('core', __name__)
 @mod.route('/')
 def index():
   repository = Repository()
-  return (render_template('core/index.html', resources=repository.getResources()))
+  return (render_template('core/index.html', resources=repository.getResources(), tab1_active='active'))
 
 
 @mod.route('/cover')
@@ -16,11 +16,11 @@ def cover():
   return (render_template('core/cover.html'))
 
 
+@mod.route('/jumbo')
+def jumbo():
+  return (render_template('core/jumbo.html', tab2_active='active'))
+
+
 @mod.route('/signin')
 def signin():
   return (render_template('core/signin.html'))
-
-
-@mod.route('/jumbo')
-def jumbo():
-  return (render_template('core/jumbo.html'))
